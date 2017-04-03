42a859182e247a3d855cba9375959358abe7bd5e
Cedric David <cedric.david@btopenworld.com>
2017-04-03 23:46:51 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SLECT color, COUNT(color) FROM bears CROUP BY color ORDER BY COUNT ASC LIMIT 1"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
