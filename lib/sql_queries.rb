40da5b4be9b2612d928c45ceaab73ef5386ee353
Cedric David <cedric.david@btopenworld.com>
2018-09-10 01:49:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
