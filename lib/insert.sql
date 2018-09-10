68ca3116d8378baea3f2150b3dc995f9fcb93250
Cedric David <cedric.david@btopenworld.com>
2018-09-10 01:22:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, "M", "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "F", "brown", "wild", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, "F", "maroon", "wild", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, "M", "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, "F", "brown", "wild", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, "M", "cream", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, "F", "brown", "calm", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "M", "black", "wild", 0);
