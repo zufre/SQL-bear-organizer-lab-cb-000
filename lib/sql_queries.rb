2a5a66f57709c66294be7c1cf5e96159b3a7ff8f
Cedric David <cedric.david@btopenworld.com>
2018-09-10 01:33:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bearsWHERE gender = female
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
